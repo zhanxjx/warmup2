5122213c258918fc4a33a76f7533a41db2d857f3
Xingjian Zhan <zxj@me.com>
2013-02-10 15:20:44 -0800

    modify unit tests

---
+++ serverTest.py
@@ -0,0 +1,100 @@
+import unittest
+import sys
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = "warmup1.settings"
+from warmup1.views import *
+
+SUCCESS  = 1
+ERR_BAD_CREDENTIALS = -1
+ERR_USER_EXISTS = -2
+ERR_BAD_USERNAME = -3
+ERR_BAD_PASSWORD = -4
+
+class UnitTests(unittest.TestCase):
+
+	def setUp(self):
+		reset()
+
+	def testAddSingleUser(self):
+		self.assertEquals(SUCCESS, add("user1", "password"))
+	
+	def testAddSameUser(self):
+		self.assertEquals(SUCCESS, add("user1", "password"))
+		self.assertEquals(ERR_USER_EXISTS, add("user1", "password"))
+		self.assertEquals(ERR_USER_EXISTS, add("user1", "password"))
+
+	def testAddMultipleUser(self):
+		self.assertEquals(SUCCESS, add("user1", "password"))
+		self.assertEquals(SUCCESS, add("user2", "password"))
+		self.assertEquals(SUCCESS, add("user3", "password"))
+	
+	def testAddUserWithEmptyUsername(self):
+		self.assertEquals(ERR_BAD_USERNAME, add("", "password"))
+
+	def testAddUserWithEmptyPassword(self):
+		self.assertEquals(SUCCESS, add("user1", ""))
+	
+	def testAddUserWithLongUsername(self):
+		self.assertEquals(ERR_BAD_USERNAME, add("user1"*128, "password"))
+	
+	def testAddUserWithLongPassword(self):
+		self.assertEquals(ERR_BAD_PASSWORD, add("user1", "password"*128))
+	
+	def testLoginSingleUserOneTime(self):
+		add("user1", "password")
+		self.assertEquals(2, login("user1", "password"))
+
+	def testLoginSingleUserMultipleTimes(self):
+		add("user1", "password")
+		login("user1", "password")
+		login("user1", "password")
+		login("user1", "password")
+		login("user1", "password")
+		login("user1", "password")
+		login("user1", "password")
+		self.assertEquals(8, login("user1", "password"))
+	
+	def testLoginMultipleUsersOneTime(self):
+		add("user1", "password")
+		add("user2", "password")
+		add("user3", "password")
+		self.assertEquals(2, login("user1", "password"))
+		self.assertEquals(2, login("user2", "password"))
+		self.assertEquals(2, login("user3", "password"))
+		
+	def testLoginMultipleUsersMultipleTimes(self):
+		add("user1", "password")
+		add("user2", "password")
+		add("user3", "password")
+		login("user1", "password")
+		login("user1", "password")
+		self.assertEquals(4, login("user1", "password"))
+		self.assertEquals(2, login("user2", "password"))
+		login("user3", "password")
+		login("user3", "password")
+		login("user3", "password")
+		login("user3", "password")
+		login("user3", "password")
+		login("user3", "password")
+		login("user3", "password")
+		self.assertEquals(9, login("user3", "password"))
+		
+	def testLoginWithEmptyUsername(self):
+		self.assertEquals(ERR_BAD_USERNAME, login("", "password"))
+
+	def testLoginWithEmptyPassword(self):
+		add("user1", "")
+		self.assertEquals(2, login("user1", ""))
+	
+	def testLoginWithLongUsername(self):
+		self.assertEquals(ERR_BAD_USERNAME, login("user1"*128, "password"))
+	
+	def testLoginWithLongPassword(self):
+		self.assertEquals(ERR_BAD_PASSWORD, login("user1", "password"*128))
+		
+	def testLoginWithWrongPassword(self):
+		add("user1", "password")
+		self.assertEquals(ERR_BAD_CREDENTIALS, login("user1", "password1"))
+
+if __name__ == "__main__":
+	unittest.main()
---
+++ testAdditional.py
@@ -0,0 +1,133 @@
+import unittest
+import os
+from testLib import RestTestCase
+
+SUCCESS = 1
+ERR_BAD_CREDENTIALS = -1
+ERR_USER_EXISTS = -2
+ERR_BAD_USERNAME = -3
+ERR_BAD_PASSWORD = -4
+
+class TestUnit(RestTestCase):
+
+	def testUnit(self):
+		respData = self.makeRequest("/TESTAPI/unitTests", method="POST")
+		self.assertTrue('output' in respData)
+		print ("Unit tests output:\n" + "\n***** ".join(respData['output'].split("\n")))
+		self.assertTrue('totalTests' in respData)
+		print "***** Reported " + str(respData['totalTests']) + " unit tests"
+		minimumTests = 10
+		self.assertTrue(respData['totalTests'] >= minimumTests, "at least "+str(minimumTests)+" unit tests. Found only " + str(respData['totalTests']) + ".")
+		self.assertEquals(0, respData['nrFailed'])
+
+class TestBackend(RestTestCase):
+	def assertResponse(self, respData, count = None, errCode = SUCCESS):
+		expected = {'errCode' : errCode}
+		if count is not None:
+			expected['count']  = count
+		self.assertDictEqual(expected, respData)
+	
+	def testAddSingleUser(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, count=1)
+	
+	def testAddSameUser(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, count=1)
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, errCode=ERR_USER_EXISTS)
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, errCode=ERR_USER_EXISTS)
+
+	def testAddMultipleUser(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, count=1)
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user2', 'password': 'password'})
+		self.assertResponse(respData, count=1)
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user3', 'password': 'password'})
+		self.assertResponse(respData, count=1)
+	
+	def testAddUserWithEmptyUsername(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': '', 'password': 'password'})
+		self.assertResponse(respData, errCode=ERR_BAD_USERNAME)
+
+	def testAddUserWithEmptyPassword(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': ''})
+		self.assertResponse(respData, count=1)
+	
+	def testAddUserWithLongUsername(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1'*128, 'password': 'password'})
+		self.assertResponse(respData, errCode=ERR_BAD_USERNAME)
+	
+	def testAddUserWithLongPassword(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'*128})
+		self.assertResponse(respData, errCode=ERR_BAD_PASSWORD)
+	
+	def testLoginSingleUserOneTime(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, count=2)
+
+	def testLoginSingleUserMultipleTimes(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, count=8)
+	
+	def testLoginMultipleUsersOneTime(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user2', 'password': 'password'})
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, count=2)
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user2', 'password': 'password'})
+		self.assertResponse(respData, count=2)
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		self.assertResponse(respData, count=2)
+		
+	def testLoginMultipleUsersMultipleTimes(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user2', 'password': 'password'})
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'})
+		self.assertResponse(respData, count=4)
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user2', 'password': 'password'})
+		self.assertResponse(respData, count=2)
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user3', 'password': 'password'})
+		self.assertResponse(respData, count=9)
+		
+	def testLoginWithEmptyUsername(self):
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': '', 'password': 'password'})
+		self.assertResponse(respData, errCode=ERR_BAD_USERNAME)
+
+	def testLoginWithEmptyPassword(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': ''})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': ''})
+		self.assertResponse(respData, count=2)
+	
+	def testLoginWithLongUsername(self):
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1'*128, 'password': 'password'})
+		self.assertResponse(respData, errCode=ERR_BAD_USERNAME)
+	
+	def testLoginWithLongPassword(self):
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password'*128})
+		self.assertResponse(respData, errCode=ERR_BAD_PASSWORD)
+		
+	def testLoginWithWrongPassword(self):
+		respData = self.makeRequest("/users/add", method="POST", data = {'user': 'user1', 'password': ''})
+		respData = self.makeRequest("/users/login", method="POST", data = {'user': 'user1', 'password': 'password1'})
+		self.assertResponse(respData, errCode=ERR_BAD_CREDENTIALS)
